3de4d129816d66dc219107726c0f842c0b0ed156
offensive_gk <bkmiecik12@gmail.com>
2016-07-22 23:17:25 +0200

    add form

---
+++ complete.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang='pl'>
+<head>
+	<meta charset="utf-8" />
+	<title>Monster Energy FIM Speedway World Cup 2016</title>
+</head>
+<body>
+	<h1>Monster Energy FIM Speedway World Cup 2016</h1>
+	<form>
+		<h2>[<a href="vojens.php">Event 1 (Vojens)</a>] </h2>
+		<h2>[<a href="vastervik.php">Event 2 (Västervik)</a>] </h2>
+		<h2>[<a href="raceoff.php">Race Off (Manchester)</a>] </h2>
+		<h2>[<a href="final.php">Final (Manchester)</a>] </h2>
+	</form>
+<?php
+
+
+?>
+
+</body>
+</html>
